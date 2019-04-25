d72a9cbad4a2fe1472ca9011f657c1a17b1c6215
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-04-25 12:51:59 +0200

    velta al lio

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,19 @@
 // let's go!
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import css from './styles/style.styl'
+import Main from './components/Main'
+
+export default class reduxstagram extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+render(<p><Main/></p>, document.getElementById('root'))
